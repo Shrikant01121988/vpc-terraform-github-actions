616d0eaca850d22b4e97d44fccf4e1242ba43245
Shrikant01121988 <122885065+Shrikant01121988@users.noreply.github.com>
2024-03-17 12:07:30 +0530

    Update data.tf

--- Terraform-VPC/modules/vpc/data.tf
+++ Terraform-VPC/modules/vpc/data.tf
@@ -1,4 +1,5 @@
 # data is variable that give the values that are already present in our machine
+# So the all availability zones with the state available must be given through the "data" variable 
 
 
 data "aws_availability_zones" "available" {
